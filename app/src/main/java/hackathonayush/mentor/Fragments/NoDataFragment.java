bbead1db7cbc6e3837a4fd67b749daed1ad208f5
Shrey <shreytripathi007@gmail.com>
2017-09-07 16:59:05 +0530

    Nodata fragment classes added

--- app/src/main/java/hackathonayush/mentor/Fragments/NoDataFragment.java
+++ app/src/main/java/hackathonayush/mentor/Fragments/NoDataFragment.java
@@ -1,8 +1,27 @@
 package hackathonayush.mentor.Fragments;
 
+import android.support.v4.app.Fragment;
+
+import hackathonayush.mentor.R;
+
 /**
  * Created by as on 07-Sep-17.
  */
 
-public class NoDataFragment {
+public class NoDataFragment extends MentorFragment {
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.nodata_fragment;
+    }
+
+    @Override
+    protected String setToolbarTitle() {
+        return null;
+    }
+
+    public static Fragment getNewInstance() {
+        NoDataFragment noDataFragment = new NoDataFragment();
+        return noDataFragment;
+    }
+
 }
